b15719ab9711eda30b53cd4a102481cba9486579
Per Lindgren <per.lindgren@ltu.se>
2024-09-17 16:42:33 +0200

    wip broken

--- examples/cpal2.rs
+++ examples/cpal2.rs
@@ -79,26 +79,31 @@ struct MyApp {
     consumer: C,
     in_data: [f32; FS],
     ptr: usize, // pointer in data
-    fft_in_data: [f32; FS],
-    spectrum: [Complex<f32>; FS / 2 + 1],
     r2c: Arc<dyn RealToComplex<f32>>,
     fft: Fft,
 }
+
+const NR_FFTS: usize = 2;
 
 impl MyApp {
     fn new(_cc: &eframe::CreationContext<'_>, consumer: C) -> Self {
         // make a planner
         let mut real_planner = RealFftPlanner::<f32>::new();
 
-        // create a FFT
+        // let mut r2cs = vec![];
+
+        // for i in 0..NR_FFTS {
+        //     let fft_size = FS as u32 / 2u32.pow(i as u32);
+        //     // create a FFT
+        //     r2cs.push(real_planner.plan_fft_forward(fft_size as usize));
+        // }
+
         let r2c = real_planner.plan_fft_forward(FS);
 
         Self {
             consumer,
             in_data: [0.0; FS],
             ptr: 0,
-            fft_in_data: [0.0; FS],
-            spectrum: [0.0.into(); FS / 2 + 1],
             r2c,
             fft: Fft::default(),
         }
@@ -119,23 +124,33 @@ impl eframe::App for MyApp {
 
             ui.label(format!("ptr {}", self.ptr));
 
-            self.fft_in_data[FS - self.ptr..].copy_from_slice(&self.in_data[..self.ptr]);
-            self.fft_in_data[..FS - self.ptr].copy_from_slice(&self.in_data[self.ptr..]);
+            let mut spectrums = vec![];
 
-            // most recent sample
-            assert_eq!(
-                self.in_data[(FS + self.ptr - 1) % FS],
-                *self.fft_in_data.last().unwrap()
-            );
-            // oldest sample
-            assert_eq!(self.in_data[self.ptr], *self.fft_in_data.first().unwrap());
+            let mut fft_in_data = self.r2c.make_input_vec();
+            let mut spectrum = self.r2c.make_output_vec();
 
-            // TODO: apply some window function
-            self.r2c
-                .process(&mut self.fft_in_data, &mut self.spectrum)
-                .unwrap();
+            for r2c in 1..=NR_FFTS {
+                // make a dummy real-valued signal (filled with zeros)
 
-            self.fft.ui_content(ui, &self.spectrum);
+                // make a vector for storing the spectrum
+
+                fft_in_data[r2c.len() - self.ptr..].copy_from_slice(&self.in_data[..self.ptr]);
+                fft_in_data[..r2c.len() - self.ptr].copy_from_slice(&self.in_data[self.ptr..]);
+
+                // most recent sample
+                assert_eq!(
+                    self.in_data[(FS + self.ptr - 1) % FS],
+                    *fft_in_data.last().unwrap()
+                );
+                // oldest sample
+                assert_eq!(self.in_data[self.ptr], *fft_in_data.first().unwrap());
+
+                // // TODO: apply some window function
+                r2c.process(&mut fft_in_data, &mut spectrum).unwrap();
+                spectrums.push(spectrum);
+            }
+
+            self.fft.ui_content(ui, spectrums);
 
             ctx.request_repaint();
         });
@@ -143,55 +158,52 @@ impl eframe::App for MyApp {
 }
 
 struct Fft {
-    max: f32,
-    old_norm: [f32; FS / 2 + 1],
+    // old_norm: [f32; FS / 2 + 1],
 }
 
 impl Default for Fft {
     fn default() -> Self {
         Fft {
-            max: 0.0,
-            old_norm: [0.0; FS / 2 + 1],
+            // old_norm: [0.0; FS / 2 + 1],
         }
     }
 }
 
 impl Fft {
-    pub fn ui_content(&mut self, ui: &mut Ui, fft_data: &[Complex<f32>]) -> egui::Response {
-        ui.label(format!("max {:?}", self.max));
+    pub fn ui_content(&mut self, ui: &mut Ui, spectrums: Vec<Vec<Complex<f32>>>) -> egui::Response {
+        // ui.label(format!("max {:?}", self.max));
 
         let mut size = ui.available_size();
-        size.x = size.x.max(fft_data.len() as f32);
-
         let (response, painter) = ui.allocate_painter(size, Sense::hover());
         let rect = response.rect;
         trace!("rect {:?}", rect);
 
-        let fft_stroke = Stroke::new(1.0, Color32::from_gray(255));
+        let fft_strokes = [
+            Stroke::new(1.0, Color32::from_gray(255)),
+            Stroke::new(1.0, Color32::YELLOW),
+        ];
+        for spectrum in spectrums {
+            // size.x = size.x.max(fft_data.len() as f32);
 
-        for bin in fft_data.iter().take(rect.width() as usize) {
-            if bin.norm() > self.max {
-                self.max = bin.norm();
+            // draw spectrum
+            for (i, bin) in spectrum.iter().take(rect.width() as usize).enumerate() {
+                painter.vline(
+                    i as f32 + rect.left(),
+                    Rangef::new(
+                        rect.top()
+                            + rect.height() * (1.0 - bin.norm() / (spectrum.len() as f32).sqrt()),
+                        rect.bottom(),
+                    ),
+                    fft_strokes[i],
+                );
             }
         }
 
-        // draw spectrum
-        for (i, bin) in fft_data.iter().take(rect.width() as usize).enumerate() {
-            painter.vline(
-                i as f32 + rect.left(),
-                Rangef::new(
-                    rect.top() + rect.height() * (1.0 - bin.norm() / self.max),
-                    rect.bottom(),
-                ),
-                fft_stroke,
-            );
-        }
-
-        painter.vline(
-            82.0 + rect.left(), // our E bin
-            Rangef::new(rect.top(), rect.height() / 2.0),
-            fft_stroke,
-        );
+        // painter.vline(
+        //     82.0 + rect.left(), // our E bin
+        //     Rangef::new(rect.top(), rect.height() / 2.0),
+        //     fft_stroke,
+        // );
 
         // painter.debug_rect(rect, Color32::RED, "here");
         response
