c7c9aaf6c6d3799b1616eaaa7b7abf8e8fb8656c
Per Lindgren <per.lindgren@ltu.se>
2024-09-19 18:51:34 +0200

    added speed flag

--- examples/audio_playback.rs
+++ examples/audio_playback.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
-use std::fs;
+use rodio::{Decoder, Source};
+use std::fs::File;
+use std::io::BufReader;
 use std::path::Path;
 #[derive(Parser, Debug)]
 struct Args {
@@ -10,19 +12,25 @@ struct Args {
         default_value = "./landskap_a_nameless_fool.wav"
     )]
     path: String,
+    #[clap(short = 's', long, help = "Speed", default_value_t = 1.0f32)]
+    speed: f32,
 }
 
 fn main() {
     let args = Args::parse();
     let path = &args.path;
     let path = Path::new(&path);
+    let speed = args.speed;
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-    let file = fs::File::open(path).unwrap();
-    println!("Now playing: {}", args.path);
-    let playback = stream_handle
-        .play_once(std::io::BufReader::new(file))
-        .unwrap();
-    playback.set_volume(0.5);
 
-    playback.sleep_until_end();
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open(path).unwrap());
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap().speed(speed);
+
+    println!("Now playing: {}", args.path);
+    // Play the sound directly on the device
+    stream_handle.play_raw(source.convert_samples()).unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
