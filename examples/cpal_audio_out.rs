0bcf62773773c2929354424c53332a3e19beddd4
onsdagens <pawdzi-7@student.ltu.se>
2024-09-25 16:43:52 +0200

    initial Track struct and cpal playback

--- examples/cpal_audio_out.rs
+++ examples/cpal_audio_out.rs
@@ -1,22 +1,16 @@
 use std::{
     borrow::{Borrow, BorrowMut},
     fs::File,
+    ops::Mul,
 };
 
 use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
     Sample,
 };
+use rodio::{Decoder, Source};
 use std::path::Path;
-use symphonia::core::{
-    audio::SampleBuffer,
-    codecs::{DecoderOptions, CODEC_TYPE_NULL},
-    errors::Error,
-    formats::FormatOptions,
-    io::MediaSourceStream,
-    meta::MetadataOptions,
-    probe::Hint,
-};
+use std::time::Duration;
 
 fn main() {
     let host = cpal::default_host();
@@ -25,124 +19,44 @@ fn main() {
 
     println!("Output device: {}", device.name().unwrap());
 
-    let config = device.default_output_config().unwrap();
-    println!("Default output config: {:?}", config);
     let file = File::open(Path::new("./amazing_grace.mp3")).unwrap();
-
-    open_audio(file);
-    run(&device, &config.into()).ok();
+    play(&device, file);
 }
 
-fn open_audio(file: File) -> Vec<f32> {
-    let file = Box::new(file);
-    let mss = MediaSourceStream::new(file, Default::default());
-    let hint = Hint::new();
-    let format_opts: FormatOptions = Default::default();
-    let metadata_opts: MetadataOptions = Default::default();
-    //let decoder_opts: DecoderOptions = Default::default();
+fn play(device: &cpal::Device, file: File) {
+    let mut sample_clock = 0;
 
-    let probed = symphonia::default::get_probe()
-        .format(&hint, mss, &format_opts, &metadata_opts)
+    let source = Decoder::new(file).unwrap();
+    let sample_rate = source.sample_rate();
+    let channels = source.channels();
+    let samples = source.convert_samples::<f32>();
+    let mut samples_vec = vec![];
+    for sample in samples {
+        samples_vec.push(sample);
+    }
+    let mut track = Track::new(sample_rate as usize, samples_vec, channels as usize);
+    let config = cpal::StreamConfig {
+        sample_rate: cpal::SampleRate(sample_rate),
+        channels,
+        buffer_size: cpal::BufferSize::Default,
+    };
+    println!("Config: {:?}", config);
+    let err_fn = |err| eprintln!("Error:{}", err);
+
+    let stream = device
+        .build_output_stream(
+            &config,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                write_data(data, track.channels, &mut || track.next_sample())
+            },
+            err_fn,
+            None,
+        )
         .unwrap();
 
-    let mut format = probed.format;
+    stream.play().ok();
 
-    let track = format
-        .tracks()
-        .iter()
-        .find(|t| t.codec_params.codec != CODEC_TYPE_NULL)
-        .expect("no supported audio tracks");
-
-    let track_id = track.id;
-
-    let dec_opts: DecoderOptions = Default::default();
-
-    // Create a decoder for the track.
-    let mut decoder = symphonia::default::get_codecs()
-        .make(&track.codec_params, &dec_opts)
-        .expect("unsupported codec");
-    let mut samples_vec = vec![];
-    loop {
-        let packet = match format.next_packet() {
-            Ok(packet) => packet,
-            Err(Error::ResetRequired) => {
-                todo!()
-            }
-            Err(Error::IoError(s)) => match s.kind() {
-                std::io::ErrorKind::UnexpectedEof => {
-                    panic!("EOF");
-                }
-                _ => {
-                    panic!();
-                }
-            },
-            Err(err) => {
-                panic!("{}", err);
-            }
-        };
-
-        while !format.metadata().is_latest() {
-            format.metadata().pop();
-        }
-
-        if packet.track_id() != track_id {
-            continue;
-        }
-
-        match decoder.decode(&packet) {
-            Ok(decoded) => {
-                let spec = decoded.spec();
-                let sampling_rate = spec.rate;
-                let samples_count = decoded.capacity();
-                println!(
-                    "Decoded {} samples at {}Hz sampling freq",
-                    samples_count, sampling_rate
-                );
-                let mut sample_buf = SampleBuffer::<f32>::new(samples_count as u64, *spec);
-
-                sample_buf.copy_planar_ref(decoded);
-
-                println!("----------------FRAME------------");
-                for sample in sample_buf.samples() {
-                    samples_vec.push(sample.clone());
-                }
-            }
-            Err(Error::DecodeError(_)) => {
-                // Failed to decode due to malformed data, skip packet
-                continue;
-            }
-            Err(err) => {
-                break;
-            }
-        }
-    }
-
-    samples_vec
-}
-
-pub fn run(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error> {
-    let sample_rate = config.sample_rate.0 as f32;
-    let channels = config.channels as usize;
-
-    // Produce a sinusoid of maximum amplitude.
-    let mut sample_clock = 0;
-    let mut next_value = move || 0.0;
-
-    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-
-    let stream = device.build_output_stream(
-        config,
-        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-            write_data(data, channels, &mut next_value)
-        },
-        err_fn,
-        None,
-    )?;
-    stream.play()?;
-
-    std::thread::sleep(std::time::Duration::from_millis(1000));
-
-    Ok(())
+    std::thread::sleep(Duration::from_secs(10));
 }
 
 fn write_data(output: &mut [f32], channels: usize, next_sample: &mut dyn FnMut() -> f32) {
@@ -151,5 +65,35 @@ fn write_data(output: &mut [f32], channels: usize, next_sample: &mut dyn FnMut()
         for sample in frame.iter_mut() {
             *sample = value;
         }
+    }
+}
+
+pub struct Track {
+    pub playhead: usize,    // in samples
+    pub sample_rate: usize, // in Hz
+    pub samples: Vec<f32>,
+    pub channels: usize,
+}
+
+impl Track {
+    pub fn new(sample_rate: usize, samples: Vec<f32>, channels: usize) -> Track {
+        Track {
+            playhead: 0,
+            sample_rate,
+            samples,
+            channels,
+        }
+    }
+
+    pub fn next_sample(&mut self) -> f32 {
+        let sample = self.samples[self.playhead];
+        self.playhead += self.channels;
+        sample
+    }
+
+    pub fn set_playhead(&mut self, transport: Duration) {
+        let secs = transport.as_secs_f32();
+        let sample_idx = (secs * self.sample_rate as f32) as usize;
+        self.playhead = sample_idx;
     }
 }
